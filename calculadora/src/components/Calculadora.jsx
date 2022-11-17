82ec7f70733af114680a0f1e6cda8d991e359e18
joaovictor91 <j-victor-franco@live.com>
2022-11-17 14:44:00 -0300

    Commit final
    Calculadora Finalizada

--- calculadora/src/components/Calculadora.jsx
+++ calculadora/src/components/Calculadora.jsx
@@ -7,16 +7,19 @@ export default function Calculadora() {
     const [num, setNum] = useState(0);
     const [oldnum, setOldNum] = useState(0);
     const [operador, setOperador] = useState();
-    
+        
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
 
     function InserirNum(e){
         
         var inserir = e.target.value;
        
         if (num === 0 ){
+           
+                setNum(inserir);
             
-            setNum(inserir);
         }
+        
         else{
             setNum(num + inserir);
         }
@@ -64,37 +67,39 @@ export default function Calculadora() {
         }
     }
 
-    return (
-        <div>
-            <Box m={5} />
-            <Container maxWidth="xs">
-            <div className='wrapper'>
-                    <Box m={12}/>
-                    <h1 className='resultado'>{num}</h1>
-                    <button onClick={Clear}>AC</button>
-                    <button onClick={MudarSinal}>+/-</button>
-                    <button onClick={Porcentagem}>%</button>
-                    <button className="orange" onClick={operatorHandler} value={"/"}>/</button>
-                    <button className="gray" onClick={InserirNum} value={7}>7</button>
-                    <button className="gray" onClick={InserirNum} value={8}>8</button>
-                    <button className="gray" onClick={InserirNum} value={9}>9</button>
-                    <button className="orange" onClick={operatorHandler} value={"X"}>X</button>
-                    <button className="gray" onClick={InserirNum} value={4}>4</button>
-                    <button className="gray" onClick={InserirNum} value={5}>5</button>
-                    <button className="gray" onClick={InserirNum} value={6}>6</button>
-                    <button className="orange" onClick={operatorHandler} value={"-"}>-</button>
-                    <button className="gray" onClick={InserirNum} value={1}>1</button>
-                    <button className="gray" onClick={InserirNum} value={2}>2</button>
-                    <button className="gray" onClick={InserirNum} value={3}>3</button>
-                    <button className="orange" onClick={operatorHandler} value={"+"}>+</button>
-                    <button className="gray" onClick={InserirNum} value={0}>0</button>
-                    <button className="gray" onClick={InserirNum} value={"."}>,</button>
-                    <button className="gray" style={{ visibility: "hidden" }} >,</button>
-                    <button className="orange" onClick={Calcular}>=</button>
-                </div>
-            </Container>
-           
-        </div>
+    return ( 
+        <body>
+            <h1>Calculadora em React</h1>
+            <div>
+                <Box m={5} />
+                <Container maxWidth="xs">
+                    <div className='wrapper'>
+                            <Box m={12}/>
+                            <h1 className='resultado'>{num}</h1>
+                            <button onClick={Clear}>AC</button>
+                            <button onClick={MudarSinal}>+/-</button>
+                            <button onClick={Porcentagem}>%</button>
+                            <button className="orange" onClick={operatorHandler} value={"/"}>/</button>
+                            <button className="gray" onClick={InserirNum} value={7}>7</button>
+                            <button className="gray" onClick={InserirNum} value={8}>8</button>
+                            <button className="gray" onClick={InserirNum} value={9}>9</button>
+                            <button className="orange" onClick={operatorHandler} value={"X"}>X</button>
+                            <button className="gray" onClick={InserirNum} value={4}>4</button>
+                            <button className="gray" onClick={InserirNum} value={5}>5</button>
+                            <button className="gray" onClick={InserirNum} value={6}>6</button>
+                            <button className="orange" onClick={operatorHandler} value={"-"}>-</button>
+                            <button className="gray" onClick={InserirNum} value={1}>1</button>
+                            <button className="gray" onClick={InserirNum} value={2}>2</button>
+                            <button className="gray" onClick={InserirNum} value={3}>3</button>
+                            <button className="orange" onClick={operatorHandler} value={"+"}>+</button>
+                            <button className="zero"  onClick={InserirNum} value={0}>0</button>
+                            <button className="gray" onClick={InserirNum} value={"."}>,</button>
+                            <button className="orange" onClick={Calcular}>=</button>
+                        </div>
+                </Container>
+                
+            </div>
+        </body>
     );
 } 
 
